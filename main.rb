9f498cc7a59ff036f1fa495fcfd3d98e05e3c0d1
JuanDBta <simplebetsas@gmail.com>
2023-08-28 18:26:57 -0500

    Add space to options

--- main.rb
+++ main.rb
@@ -5,18 +5,18 @@ def main
 
   loop do
     puts 'Please enter the number of the option:'
-    puts '1 - List all books'
-    puts '2 - List all movies'
-    puts '3 - List of games'
-    puts '4 - List all genres'
-    puts '5 - List all labels'
-    puts '6 - List all authors'
-    puts '7 - List all sources'
-    puts '8 - Add a book'
-    puts '9 - Add a music album'
+    puts ' 1 - List all books'
+    puts ' 2 - List all movies'
+    puts ' 3 - List of games'
+    puts ' 4 - List all genres'
+    puts ' 5 - List all labels'
+    puts ' 6 - List all authors'
+    puts ' 7 - List all sources'
+    puts ' 8 - Add a book'
+    puts ' 9 - Add a music album'
     puts '10 - Add a movie'
     puts '11 - Add a game'
-    puts '0 - Exit'
+    puts ' 0 - Exit'
     input = gets.chomp
     case input
     when '1'
