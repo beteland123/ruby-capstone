d6eda4e1995d0989c9832e0045aee3ce16fdefcb
JuanDBta <simplebetsas@gmail.com>
2023-08-28 18:23:07 -0500

    Fix offenses

--- main.rb
+++ main.rb
@@ -1,56 +1,48 @@
 def main
-
-    def title
-        puts
-        puts 'Catalog of My Things!!!'
-        puts
-      end
-
-    def options
-        puts 'Please enter the number of the option:'
-        puts '1 - List all books'
-        puts '2 - List all movies'
-        puts '3 - List of games'
-        puts '4 - List all genres'
-        puts '5 - List all labels'
-        puts '6 - List all authors'
-        puts '7 - List all sources'
-        puts '8 - Add a book'
-        puts '9 - Add a music album'
-        puts '10 - Add a movie'
-        puts '11 - Add a game'
-        puts '0 - Exit'
-    end
-    
+  puts
+  puts 'Catalog of My Things!!!'
+  puts
 
   loop do
-    options
+    puts 'Please enter the number of the option:'
+    puts '1 - List all books'
+    puts '2 - List all movies'
+    puts '3 - List of games'
+    puts '4 - List all genres'
+    puts '5 - List all labels'
+    puts '6 - List all authors'
+    puts '7 - List all sources'
+    puts '8 - Add a book'
+    puts '9 - Add a music album'
+    puts '10 - Add a movie'
+    puts '11 - Add a game'
+    puts '0 - Exit'
     input = gets.chomp
     case input
     when '1'
-        puts 'No books available yet'
+      puts 'No books available yet'
     when '2'
-        puts 'No movies available yet'
+      puts 'No movies available yet'
     when '3'
-        puts 'No games available yet'
+      puts 'No games available yet'
     when '4'
-        puts 'No books available yet'
+      puts 'No genres available yet'
     when '5'
-        puts 'No genres available yet'
+      puts 'No labels available yet'
     when '6'
-         puts 'No authors available yet'
+      puts 'No authors available yet'
     when '7'
-        puts 'No sources available yet'
+      puts 'No sources available yet'
     when '8'
-        print 'Enter title of the book: '
-        puts 'Enter author: '
+      print 'Enter title of the book: '
+      puts 'Enter author: '
     when '9'
-        print 'Enter title of the album: '
-        puts 'Enter artist: '
+      print 'Enter title of the album: '
+      puts 'Enter artist: '
     when '10'
-        puts 'Enter title of the movie: '
+      puts 'Enter title of the movie: '
     when '11'
-        puts 'Enter title of the game: '
+      puts 'Enter title of the game: '
     when '0'
       puts 'Goodbye!!!'
       exit
