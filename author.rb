12acdb7ce4494f86504f9e56147c0c1cbb745614
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-29 11:22:52 -0600

    Fix linters

--- author.rb
+++ author.rb
@@ -1,13 +1,14 @@
-require_relative item 
+require_relative item
 
 class Author < Item
-    attr_accessor :first_name, :last_name
-    attr_reader :item
+  attr_accessor :first_name, :last_name
+  attr_reader :item
 
-    def initialize(first_name, last_name)
-        @id = Random.rand(1..1000)
-        @first_name= first_name
-        @last_name  = last_name
-        @items = []
-    end
+  def initialize(first_name, last_name)
+    super()
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 end
