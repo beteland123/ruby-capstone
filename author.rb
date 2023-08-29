185c0a8c77768794fc7fc7b3a80356cca3c71c7d
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-29 11:22:01 -0600

    Add author class

---
+++ author.rb
@@ -0,0 +1,13 @@
+require_relative item 
+
+class Author < Item
+    attr_accessor :first_name, :last_name
+    attr_reader :item
+
+    def initialize(first_name, last_name)
+        @id = Random.rand(1..1000)
+        @first_name= first_name
+        @last_name  = last_name
+        @items = []
+    end
+end
