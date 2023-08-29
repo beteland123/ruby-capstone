acb180ad202c34cd0ef8f50efa144c5ee8064322
JuanDBta <simplebetsas@gmail.com>
2023-08-29 12:31:23 -0500

    Fix offense

--- book.rb
+++ book.rb
@@ -3,7 +3,7 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state, publish_date, archived: false)
-    super(publish_date, archived:)
+    super(publish_date, archived: false)
     @cover_state = cover_state
     @publisher = publisher
   end
