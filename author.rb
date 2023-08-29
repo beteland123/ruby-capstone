267468c00e0716a1006654134008792f1da05000
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-29 11:43:05 -0600

    Add reviewer changes

--- author.rb
+++ author.rb
@@ -1,11 +1,8 @@
-require_relative item
-
-class Author < Item
+class Author
   attr_accessor :first_name, :last_name
   attr_reader :item
 
   def initialize(first_name, last_name)
-    super()
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
