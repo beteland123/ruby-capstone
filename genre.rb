d3626e9b59634022b50d0003630d559f2ed277ef
JuanDBta <simplebetsas@gmail.com>
2023-08-29 15:30:18 -0500

    Implemented methods to MusicAlbum and Genre class

--- genre.rb
+++ genre.rb
@@ -2,9 +2,15 @@ class Genre
   attr_accessor :name
   attr_reader :items
 
-  def intialize(name)
+  def initialize(name)
     @id = Random.rand(1..1000)
     @name = name
     @items = []
+  end
+
+  def add_item(item)
+    item.is_a?(Item)
+    item.genre = self
+    @items << item
   end
 end
