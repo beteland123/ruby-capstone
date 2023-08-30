85dcf331f7868ac3733723d83db18cc385436a9f
JuanDBta <simplebetsas@gmail.com>
2023-08-30 11:56:34 -0500

    Correct tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Item do
   let(:label) { double('Label') }
   let(:item) { Item.new('04/02/1998') }
+  let(:genre) { double('Genre') }
+  let(:author) { double('Author') }
 
   describe '#can_be_archived?' do
     it 'returns true if the pulish date is more than 10 year' do
@@ -38,6 +40,38 @@ RSpec.describe Item do
       allow(label).to receive(:items).and_return([item])
       item.label = label
       expect(item.label).to eq label
+    end
+  end
+
+  describe '#genre=' do
+    it 'assigns the genre and adds the item to the genre' do
+      # Stub `items` method to return an empty array
+      allow(genre).to receive(:items).and_return([])
+      # Expect `items` method to be called once
+      expect(genre).to receive(:items).once.and_return([item])
+      item.genre = genre
+      expect(item.genre).to eq genre
+    end
+
+    it 'does not add the item to the genre if already present' do
+      # Stub `items` method to return an array with the item object
+      allow(genre).to receive(:items).and_return([item])
+      item.genre = genre
+      expect(item.genre).to eq genre
+    end
+  end
+  describe '#author=' do
+    it 'assigns the author and adds the item to the author' do
+      allow(author).to receive(:items).and_return([])
+      expect(author).to receive(:items).once.and_return([item])
+      item.author = author
+      expect(item.author).to eq author
+    end
+
+    it 'does not add the item to the author if already present' do
+      allow(author).to receive(:items).and_return([item])
+      item.author = author
+      expect(item.author).to eq author
     end
   end
 end
