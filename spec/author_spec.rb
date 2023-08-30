9f25bf1208193af8c72370106e66ee148060338f
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-30 13:58:11 -0600

    Correct offenses

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Author do
-  let(:author) { Author.new("John", "Doe") }
-  let(:item) { Item.new("2022-08-30") } 
+  let(:author) { Author.new('John', 'Doe') }
+  let(:item) { Item.new('2022-08-30') }
 
-  describe "#add_item" do
+  describe '#add_item' do
     it "associates the item with the author and adds it to the author's items" do
-      author.add_item(item) 
+      author.add_item(item)
 
       expect(item.author).to eq(author)
       expect(author.instance_variable_get(:@items)).to include(item)
