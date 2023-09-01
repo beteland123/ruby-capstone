c8c8848358e4d2f4041e97050b62e5c3b878a2d3
beteland123 <beteland23@gmail.com>
2023-09-01 16:10:10 +0300

    fix offense

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,8 @@ describe Game do
   describe '#can_be_archived?' do
     context 'when the game is not archived and last played more than 2 years ago' do
       it 'returns true' do
-        Time.now
-        (3 * 365 * 24 * 60 * 60) # 3 years ago
-        game = Game.new('04/3/2023', true, '2021-01-01')
+        last_played_at = Time.now - (3 * 365 * 24 * 60 * 60) # 3 years ago
+        game = Game.new('2021-01-01', true, last_played_at)
 
         expect(game.can_be_archived?).to be(false)
       end
@@ -12,9 +11,8 @@ describe Game do
 
     context 'when the game is not archived and last played less than 2 years ago' do
       it 'returns false' do
-        Time.now
-        (1 * 365 * 24 * 60 * 60) # 1 year ago
-        game = Game.new('04/3/2023', true, '2021-01-01')
+        last_played_at = Time.now - (1 * 365 * 24 * 60 * 60) # 1 year ago
+        game = Game.new('2021-01-01', true, last_played_at)
 
         expect(game.can_be_archived?).to be(false)
       end
@@ -22,9 +20,8 @@ describe Game do
 
     context 'when the game is already archived' do
       it 'returns true' do
-        Time.now
-        (3 * 365 * 24 * 60 * 60) # 3 years ago
-        game = Game.new('04/3/2023', true, '2021-01-01')
+        last_played_at = Time.now - (3 * 365 * 24 * 60 * 60) # 3 years ago
+        game = Game.new('2021-01-01', true, last_played_at)
         game.archive
 
         expect(game.can_be_archived?).to be(false)
