426c8edf418274bb2fc378ed318f51bb5fbeb9f9
beteland123 <beteland23@gmail.com>
2023-09-02 00:14:53 +0300

    add schema for item , label and book class

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE catalog_of_my_things;
+CREATE TABLE item (
+    id SERIAL PRIMARY KEY NOT NULL,
+    publish_date DATE,
+    archived BOOLEAN,
+    genre  INTEGER REFERENCES genre(id),
+    author INTEGER REFERENCES author(id),
+    label INTEGER REFERENCES label(id),
+    source INTEGER REFERENCES source(id)
+);
+
+CREATE TABLE label (
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    color VARCHAR(50) NOT NULL
+);
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY NOT NULL,
+    publish_date DATE,
+    archived BOOLEAN,
+    publisher VARCHAR(50) NOT NULL,
+    cover_state VARCHAR(50) NOT NULL,
+    genre  INTEGER REFERENCES genre(id),
+    author INTEGER REFERENCES author(id),
+    label INTEGER REFERENCES label(id),
+    source INTEGER REFERENCES source(id)
+);
